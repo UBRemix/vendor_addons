5c9ad2f7fa6a64491a39ff0a104ea69f9e543d90
mydongistiny <jaysonedson@gmail.com>
2016-08-12 17:18:35 -0700

    addons: Bring in Substratum 210 with recreate prop
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -1,7 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Substratum. Use recreate
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.substratum.recreate=true
+
 # Prebuilts
 PRODUCT_PACKAGES += \
     BenzoBlock \
-    SnapBrowser
+    SnapBrowser \
+    Substratum

51586722d1dab746a3bb614026decf37dfc7da6c
mydongistiny <jaysonedson@gmail.com>
2016-08-12 17:20:50 -0700

    addons: Bring in custom Kernel Adiutor 0.9.28.1 beta.br1
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Hide KA launcher
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kerneladiutor.hide=true
+
 # Substratum. Use recreate
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.recreate=true
@@ -8,5 +12,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Prebuilts
 PRODUCT_PACKAGES += \
     BenzoBlock \
+    KernelAdiutor \
     SnapBrowser \
     Substratum

1f7db25d4034326e8f4094be41ac5c9f11e4da3b
mydongistiny <jaysonedson@gmail.com>
2016-09-11 13:50:57 -0700

    config: Remove Kernel Adiutor hide launcher prop
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Hide KA launcher
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kerneladiutor.hide=true
-
 # Substratum. Use recreate
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.recreate=true
